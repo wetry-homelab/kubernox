af71e698d9391fc3f6e9371da1d73b2b7c627cad
Patrick Grasseels <grasseelsp@hotmail.com>
2021-02-18 23:34:39 +0100

    Add node pane with charts

---
+++ src/Kubernox.UI/Pages/K3SCluster/ClusterChart.razor.cs
@@ -0,0 +1,62 @@
+﻿using AntDesign.Charts;
+using Infrastructure.Contracts.Response;
+using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+
+namespace Kubernox.UI.Pages.K3SCluster
+{
+    public partial class ClusterChart : ComponentBase
+    {
+        public enum ChartType
+        {
+            CPU,
+            RAM
+        };
+
+        [Parameter]
+        public List<SimpleMetricItemResponse> Metrics { get; set; }
+
+        [Parameter]
+        public string Title { get; set; }
+
+        [Parameter]
+        public ChartType Type { get; set; }
+
+        protected PercentStackedAreaConfig chartMetricConfiguration;
+
+        protected override void OnAfterRender(bool firstRender)
+        {
+            if (firstRender)
+            {
+                chartMetricConfiguration = new PercentStackedAreaConfig
+                {
+                    Title = new Title
+                    {
+                        Visible = true,
+                        Text = Title
+                    },
+                    XField = "DateValue",
+                    YField = GetYFieldValue(Type),
+                    Color = new[] { "#82d1de" },
+                    AreaStyle = new GraphicStyle
+                    {
+                        FillOpacity = 0.7M
+                    }
+                };
+
+                StateHasChanged();
+            }
+
+            base.OnAfterRender(firstRender);
+        }
+
+        private string GetYFieldValue(ChartType type)
+        {
+            if (type == ChartType.CPU)
+                return "CpuValue";
+
+            return "RamValue";
+        }
+    }
+}
