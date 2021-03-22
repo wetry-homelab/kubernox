ebf1f819223706dcfee6053a2d214b9425ee8a61
Patrick Grasseels <grasseelsp@hotmail.com>
2021-03-22 14:52:27 +0100

    Add verification date

---
+++ src/Infrastructure.Persistence/Migrations/20210322135047_Add domain validation date.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Infrastructure.Persistence.Migrations
+{
+    public partial class Adddomainvalidationdate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "ValidationDate",
+                table: "DomainName",
+                type: "timestamp without time zone",
+                nullable: true);
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "CreateAt",
+                table: "Cluster",
+                type: "timestamp without time zone",
+                nullable: false,
+                defaultValue: new DateTime(2021, 3, 22, 13, 50, 46, 846, DateTimeKind.Utc).AddTicks(1870),
+                oldClrType: typeof(DateTime),
+                oldType: "timestamp without time zone",
+                oldDefaultValue: new DateTime(2021, 3, 22, 11, 45, 58, 145, DateTimeKind.Utc).AddTicks(1592));
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ValidationDate",
+                table: "DomainName");
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "CreateAt",
+                table: "Cluster",
+                type: "timestamp without time zone",
+                nullable: false,
+                defaultValue: new DateTime(2021, 3, 22, 11, 45, 58, 145, DateTimeKind.Utc).AddTicks(1592),
+                oldClrType: typeof(DateTime),
+                oldType: "timestamp without time zone",
+                oldDefaultValue: new DateTime(2021, 3, 22, 13, 50, 46, 846, DateTimeKind.Utc).AddTicks(1870));
+        }
+    }
+}
